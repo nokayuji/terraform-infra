e6ec0faa517207a61b9a4764eca828979169788f
nokayuji <nokayuji0928@gmail.com>
2025-08-20 12:52:36 +0900

    t

--- modules/VPC/test.ebs.tf
+++ modules/VPC/test.ebs.tf
@@ -2,4 +2,5 @@ resource "aws_ebs_volume" "test" {
   availability_zone = "ap-northeast-1a"
   size              = var.size
   encrypted         = var.enc # ❌ TFLintに怒られる
+  volume_type       = "invalid_type"
 }
