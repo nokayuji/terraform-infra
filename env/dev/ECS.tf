ecbd1ce099718999734b0058b335e136d693df72
nokayuji <nokayuji0928@gmail.com>
2025-08-22 05:04:20 +0900

    修正

--- env/dev/ECS.tf
+++
@@ -1,9 +0,0 @@
-# module "ECS" {
-#   source               = "../modules/WEB/ECS"
-#   public_subnet_ids    = module.VPC.public_subnet_ids
-#   public_subnet_ids_2  = module.VPC.public_subnet_ids_2
-#   private_subnet_ids   = module.VPC.private_subnet_ids
-#   private_subnet_ids_2 = module.VPC.private_subnet_ids_2
-#   vpc_id               = module.VPC.vpc_id
-#   desired_count        = var.desired_count
-# }
--- modules/VPC/testec2.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "ec2" {
-  ami           = "ami-0ca38c7440de1749aa"
-  instance_type = "t1.micro"
-  tags = {
-    Name = "tflint-test"
-  }
-}
