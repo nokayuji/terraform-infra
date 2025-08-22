eeacc8f5e7a1799634732b9041431cd9c9ec934f
nokayuji <nokayuji0928@gmail.com>
2025-08-22 09:55:33 +0900

    Feature/cicd test (#3)
    * r
    
    * CI確認
    
    * d
    
    * d
    
    * f
    
    * d
    
    * d
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * CI確認
    
    * c
    
    * testci
    
    * え
    
    * ci
    
    * s
    
    * t
    
    * t
    
    * ci
    
    * う
    
    * j
    
    * え
    
    * v
    
    * v
    
    * a
    
    * t
    
    * test
    
    * CI確認
    
    * CI確認
    
    * CI test
    
    * CI_TEST
    
    * CITEST
    
    * CITEST
    
    * test
    
    * d
    
    * a
    
    * a
    
    * t
    
    * d
    
    * d
    
    * CI
    
    * f
    
    * f
    
    * CICDの実施
    
    * CICDの実施
    
    * t
    
    * s
    
    * d
    
    * CD
    
    * CD
    
    * 修正
    
    * d
    
    * x
    
    * net

---
+++ env/dev/network.tf
@@ -0,0 +1,17 @@
+module "network" {
+  source = "../../modules/network"
+
+  sys                       = var.sys
+  env                       = var.env
+  az_a                      = var.az_a
+  az_c                      = var.az_c
+  az_d                      = var.az_d
+  cidr_block                = var.cidr_block
+  cidr_sunet_block_public_a = var.cidr_sunet_block_public_a
+  cidr_sunet_block_public_c = var.cidr_sunet_block_public_c
+  # cidr_sunet_block_private_a = var.cidr_sunet_block_private_a
+  # cidr_sunet_block_private_c = var.cidr_sunet_block_private_c
+  cidr_rt_block = var.cidr_rt_block
+  #   subnet_tag                 = var.subnet_tag.private
+  vpc_id = var.vpc_id
+}
