994589b6118867bfa43ee272ef6dae78e3a8b98c
nokayuji <nokayuji0928@gmail.com>
2025-08-22 17:56:01 +0900

    CD

--- modules/test_html_image_delivery/aws_s3.tf
+++ modules/test_html_image_delivery/aws_s3.tf
@@ -42,7 +42,7 @@ resource "aws_s3_object" "index_delivery" {
   bucket       = aws_s3_bucket.image_delivery.id
   key          = "index.html"
   source       = "${path.module}/index.html"
-  acl          = "public-read"
+  #acl          = "public-read"
   content_type = "text/html"
   depends_on   = [aws_s3_bucket_ownership_controls.image_delivery]
 }
@@ -51,7 +51,7 @@ resource "aws_s3_object" "image01_delivery" {
   bucket       = aws_s3_bucket.image_delivery.id
   key          = "image01.jpeg"
   source       = "${path.module}/image01.jpeg"
-  acl          = "public-read"
+  #acl          = "public-read"
   content_type = "image01/jpeg"
   depends_on   = [aws_s3_bucket_ownership_controls.image_delivery]
 }
@@ -60,7 +60,7 @@ resource "aws_s3_object" "image02_delivery" {
   bucket       = aws_s3_bucket.image_delivery.id
   key          = "image02.jpeg"
   source       = "${path.module}/image02.jpeg"
-  acl          = "public-read"
+  #acl          = "public-read"
   content_type = "image02/jpeg"
   depends_on   = [aws_s3_bucket_ownership_controls.image_delivery]
 }
