6ff910e57b8f2da3ae7b5281c4ed452ca6ec0a80
nokayuji <nokayuji0928@gmail.com>
2025-08-22 15:25:45 +0900

    feature/self-hosted-runnner

---
+++ modules/self_hosted_runner/aws_securitygroup.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "runner_ec2_sg" {
+  name   = "${var.sys}-${var.env}-runner-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.sys}-${var.env}-runner-sg"
+  }
+}
