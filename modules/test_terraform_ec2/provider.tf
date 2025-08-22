ff851b726769ddc30c231b9b360a013014f0a8af
nokayuji <nokayuji0928@gmail.com>
2025-08-22 18:20:20 +0900

    CD

--- modules/test_terraform_ec2/provider.tf
+++ modules/test_terraform_ec2/provider.tf
@@ -9,9 +9,9 @@ terraform {
       source  = "hashicorp/tls"
       version = "~> 4.0"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
+    # local = {
+    #   source  = "hashicorp/local"
+    #   version = "~> 2.0"
+    # }
   }
 }
